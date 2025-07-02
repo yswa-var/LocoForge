a3bcee33a752ede66192ee15233f071527abb65f
yashaswa-beepboop <yashaswa.varshney@hyprbots.com>
2025-07-02 14:49:59 +0530

    Fix: context

--- test_key.py
+++ test_key.py
@@ -1,6 +1,5 @@
 import openai
 
-openai.api_key = "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"
-
+openai.api_key = ""
 models = openai.models.list()
 print(models)
